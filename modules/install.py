1f7c851b45367e2af7e4575e1d3a48c1cb564fc2
skunk-ink <murray.crawford85@gmail.com>
2022-05-06 04:52:35 -0700

    skunk-ink

--- modules/install.py
+++ modules/install.py
@@ -32,6 +32,9 @@ class run():
         DATA_PATH = PATH + '/data/'      # Data Directory Path
         HSD_PATH = PATH + '/hsd/'
         SKYNET_PATH = PATH + '/skynet-webportal/'
+        ANSIBLE_PLAYBOOKS_PATH = PATH + '/ansible-playbooks/'
+        ANSIBLE_PRIVATE_PATH = PATH + '/ansible-private/'
+        POWERDNS_PATH = PATH + '/pdns/'
         LOG_FILE = DATA_PATH + "install.log"
 
         DEPENDS = open("DEPENDS", "r")
@@ -58,7 +61,7 @@ class run():
                     addLinuxPackage = True
                     addPythonPackage = False
                     addAdamsPackage = False
-                    print(colours.red(self, "\nInstalling Linux Dependencies..."))
+                    print(colours.red(self, "Installing Linux Dependencies..."))
 
                 elif package.startswith("# WINDOWS"):
                     addWinPackage = False
@@ -89,13 +92,13 @@ class run():
                 elif addLinuxPackage == True:
 
                     if package != "":
-                        print(colours.green(self, " [+] ") + "Installing APT Dependency: " + str(package))
+                        print(colours.green(self, " [+] ") + "Installing " + str(package))
                         subprocess.run(["sudo", "apt", "-qqq", "install", "-y", package], check=True)
 
                 elif addPythonPackage == True:
 
                     if package != "":
-                        print(colours.green(self, " [+] ") + "Installing PIP Dependency: " + str(package))
+                        print(colours.green(self, " [+] ") + "Installing " + str(package))
                         subprocess.run(["pip", "-q", "install", package], check=True)
 
                 elif addAdamsPackage == True:
@@ -103,21 +106,51 @@ class run():
                     if package != "":
                         if package.endswith("skynet-webportal.git"):
                             if os.path.isdir(SKYNET_PATH) == False:
-                                print(colours.green(self, " [+] ") + "Installing A.D.A.M.S Dependency: " + str(package))
+                                print(colours.green(self, " [+] ") + "Installing Skynet Webportal")
+                                subprocess.run(["git", "clone", package], check=True)
+                                #subprocess.run(["npm", "install", "yarn"], cwd=SKYNET_PATH)
+                                #subprocess.run(["yarn", "build"], cwd=SKYNET_PATH)
+                                print()
+                                sleep(5)
+                            else:
+                                print(colours.red(self, " [-] ") + "Skynet Webportal Installation Detected!")
+
+                        elif package.endswith("ansible-playbooks.git"):
+                            if os.path.isdir(ANSIBLE_PLAYBOOKS_PATH) == False:
+                                print(colours.green(self, " [+] ") + "Installing Ansible-Playbooks")
                                 subprocess.run(["git", "clone", package], check=True)
                                 print()
                             else:
-                                print(colours.red(self, " [-] ") + "Skynet Portal Installation Detected!")
+                                print(colours.red(self, " [-] ") + "Ansible-Playbooks Installation Detected!")
                                 
-
+                        elif package.endswith("ansible-private-sample.git"):
+                            if os.path.isdir(ANSIBLE_PRIVATE_PATH) == False:
+                                print(colours.green(self, " [+] ") + "Installing Ansible-Private")
+                                subprocess.run(["git", "clone", package, "ansible-private"], check=True)
+                                print()
+                            else:
+                                print(colours.red(self, " [-] ") + "Ansible-Private Installation Detected!")
+                                
                         elif package.endswith("hsd.git"):
                             if os.path.isdir(HSD_PATH) == False:
-                                print(colours.green(self, " [+] ") + "Installing A.D.A.M.S Dependency: " + str(package))
+                                print(colours.green(self, " [+] ") + "Installing Handshake Daemon")
                                 subprocess.run(["git", "clone", package], check=True)
                                 subprocess.run(["npm", "install"], cwd=HSD_PATH)
                                 print()
                             else:
-                                print(colours.red(self, " [-] ") + "HSD Installation Detected!")
+                                print(colours.red(self, " [-] ") + "Handshake Daemon Installation Detected!")
+
+                        elif package.endswith("pdns.git"):
+                            if os.path.isdir(POWERDNS_PATH) == False:
+                                print(colours.green(self, " [+] ") + "Installing PowerDNS")
+                                subprocess.run(["git", "clone", package], check=True)
+                                subprocess.run(["autoreconf", "-vi"], cwd=POWERDNS_PATH)
+                                subprocess.run(["./configure"], cwd=POWERDNS_PATH)
+                                #subprocess.run(["make", "html-docs"], cwd=POWERDNS_PATH + "docs")
+                                print()
+                            else:
+                                print(colours.red(self, " [-] ") + "PowerDNS Installation Detected!")
+                                
                             
                 
         # Install packages for Windows
