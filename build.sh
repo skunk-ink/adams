b6831d9007b11bbd2e3dacb838be200346a5d723
skunk-ink <murray.crawford85@gmail.com>
2022-05-09 21:41:12 -0700

    skunk-ink

--- build.sh
+++ build.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-echo "Installing APT Dependencies: python3, python-pip"
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+echo -e "${GREEN}[+]${NC} Installing APT Dependencies: python3, python-pip"
 sudo apt -qqq install -y python3 python3-pip
 sleep 1
 
-echo "Installing Python Dependencies: pyinstaller, getch"
+echo -e "${GREEN}[+]${NC} Installing Python Dependencies: pyinstaller, getch"
 pip install -qq -U pyinstaller
 pip install -qq  getch
 export PATH=~/.local/bin:$PATH
 sleep 1
 
-echo "Building A.D.A.M.S. binary..."
-pyinstaller --name adams --distpath ./ -F modules/main.py
+echo -e "${GREEN}[+]${NC} Building A.D.A.M.S. binary..."
+pyinstaller -qq --name adams --distpath ./ -F modules/main.py
 sleep 1
