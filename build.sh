a0edefe86a54d8a68e310b1ab7a37642ae369025
skunk-ink <murray.crawford85@gmail.com>
2022-05-09 20:25:19 -0700

    Updated build.sh script

--- build.sh
+++ build.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-echo "Installing Dependencies... Please wait."
+echo "Installing APT Dependencies: python3, python-pip"
 sudo apt -qqq install -y python3 python3-pip
+sleep 1
+
+echo "Installing Python Dependencies: pyinstaller, getch"
 pip install -qq -U pyinstaller
 pip install -qq  getch
 export PATH=~/.local/bin:$PATH
+sleep 1
 
+echo "Building A.D.A.M.S. binary..."
 pyinstaller --name adams --distpath ./ -F modules/main.py
+sleep 1
