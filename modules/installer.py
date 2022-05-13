4c6fa21187cb6bf064ba2f2c97fea8f640d3c4dd
skunk-ink <murray.crawford85@gmail.com>
2022-05-12 23:23:38 -0700

    Enabled Dependencies

--- modules/installer.py
+++ modules/installer.py
@@ -30,7 +30,7 @@ from display import clear_screen
 
 disableInstaller = False
 disableSubprocesses = False
-disableDependencyInstall = True
+disableDependencyInstall = False
 
 if platform == "linux":
     from getch import getch as getch
